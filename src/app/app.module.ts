a97309aa3726f3a2cd0e4c26c96f7881b5a3e495
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-04-23 04:20:08 +0000

    Added navbar and sheep.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SheepComponent } from './components/sheep/sheep.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SheepComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/sheep/sheep.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bf-sheep',
+  templateUrl: './sheep.component.html',
+  styleUrls: ['./sheep.component.scss']
+})
+export class SheepComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
