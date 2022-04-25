598322dd6af4663999becd954eeea34bafb53b31
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-04-25 11:31:53 +0000

    Removed play details component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { BattleGridComponent } from './components/battle-grid/battle-grid.component';
 import { ShipComponent } from './components/ship/ship.component';
 import { CargoPortComponent } from './components/cargo-port/cargo-port.component';
-import { PlayDetailsComponent } from './components/play-details/play-details.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatMenuModule } from '@angular/material/menu';
@@ -28,7 +27,6 @@ const matModules = [
     BattleGridComponent,
     ShipComponent,
     CargoPortComponent,
-    PlayDetailsComponent,
     CurrentPlayerComponent
   ],
   imports: [
