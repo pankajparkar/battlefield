0a17568ba152b403db93ed677dc308539e3219d7
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-04-25 06:51:47 +0000

    Implementation of storage service.

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -6,4 +6,47 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
+
+  get(key: string) {
+    const result = localStorage.getItem(key);
+    try {
+      return JSON.parse(result ?? '{}');
+    } catch (error) {
+      return result;
+    }
+  }
+
+  set(key: string, value: unknown) {
+    let valueToBeStored = value;
+    try {
+      valueToBeStored = JSON.stringify(value);
+    } finally {
+      localStorage.setItem(key, valueToBeStored as string);
+    }
+  }
+
+  getByProperty(key: string, prop: string) {
+    const result = JSON.parse(localStorage.getItem(key) || '{}');
+    return result[prop];
+  }
+
+  setByProperty(key: string, prop: string, propValue: unknown) {
+    const result = JSON.parse(localStorage.getItem(key) || '{}');
+    result[prop] = propValue || '';
+    this.set(key, result);
+  }
+
+  remove(key: string) {
+    try {
+      localStorage.removeItem(key);
+    } catch (error) {
+      console.error('Error removing data.');
+    }
+  }
+
+  removeProperty(key: string, prop: string) {
+    const result = JSON.parse(localStorage.getItem(key) || '{}');
+    delete result[prop];
+    this.set(key, result);
+  }
 }
