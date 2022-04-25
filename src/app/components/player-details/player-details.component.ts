6614384eb0ab201eb76f276fe0ed3ad97316c521
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-04-25 11:32:13 +0000

    Added player details component.

---
+++ src/app/components/player-details/player-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bf-player-details',
+  templateUrl: './player-details.component.html',
+  styleUrls: ['./player-details.component.scss']
+})
+export class PlayerDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
