6f6fd2b4a94673a1109127f14eb824e28d0103a3
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-05-02 23:39:24 +0800

    Moved battle platform code separately.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,31 +11,4 @@ import { FleetPositionsService } from './services/fleet-positions.service';
 })
 export class AppComponent {
 
-  players$: Observable<Player[]> = this.fleetPosition.playersObservable;
-  winner$ = this.fleetPosition.checkWinner$;
-  currentPlayer$ = this.fleetPosition.currentPlayer$;
-  positions$ = combineLatest([
-    this.players$,
-    this.currentPlayer$,
-  ]).pipe(
-    map(
-      ([players, currentPlayer]) => players.length > 0 ?
-        currentPlayer.positions :
-        null
-    ),
-  );
-  showPort$ = this.positions$.pipe(
-    map(positions => ([
-      ...(positions?.horizontal ?? []),
-      ...(positions?.vertical ?? []),
-    ].length !== 8))
-  )
-
-  constructor(
-    private fleetPosition: FleetPositionsService,
-  ) { }
-
-  isCurrentPlayer(player: Player, currentPlayer: Player) {
-    return player.id !== currentPlayer.id
-  }
 }
--- src/app/components/battle-platform/battle-platform.component.ts
+++ src/app/components/battle-platform/battle-platform.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { combineLatest, map, Observable } from 'rxjs';
+import { Player } from 'src/app/models';
+import { FleetPositionsService } from 'src/app/services';
 
 @Component({
   selector: 'bf-battle-platform',
   templateUrl: './battle-platform.component.html',
   styleUrls: ['./battle-platform.component.scss']
 })
-export class BattlePlatformComponent implements OnInit {
+export class BattlePlatformComponent {
 
-  constructor() { }
+  players$: Observable<Player[]> = this.fleetPosition.playersObservable;
+  winner$ = this.fleetPosition.checkWinner$;
+  currentPlayer$ = this.fleetPosition.currentPlayer$;
+  positions$ = combineLatest([
+    this.players$,
+    this.currentPlayer$,
+  ]).pipe(
+    map(
+      ([players, currentPlayer]) => players.length > 0 ?
+        currentPlayer.positions :
+        null
+    ),
+  );
+  showPort$ = this.positions$.pipe(
+    map(positions => ([
+      ...(positions?.horizontal ?? []),
+      ...(positions?.vertical ?? []),
+    ].length !== 8))
+  )
 
-  ngOnInit(): void {
+  constructor(
+    private fleetPosition: FleetPositionsService,
+  ) { }
+
+  isCurrentPlayer(player: Player, currentPlayer: Player) {
+    return player.id !== currentPlayer.id
   }
 
 }
