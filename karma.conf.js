323ef39884f42316326536ad751c619049297507
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-05-06 00:35:04 +0800

    Battlefield => battleship

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/battlefield'),
+      dir: require('path').join(__dirname, './coverage/battleship'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'battlefield'`, () => {
+  it(`should have as title 'battleship'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('battlefield');
+    expect(app.title).toEqual('battleship');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('battlefield app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('battleship app is running!');
   });
 });
